af3a12ffd5b5e1b676576849ff34e6ae053a7ee9
JonielRocha <jonielrocha88@gmail.com>
2019-10-07 21:48:59 -0300

    Insert Components Layout

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+<div classeName="Profile-wrapper">   
+<h1>Joniel Rocha</h1>
+<h2>Analyst IT</h2>
+<p> Work the in universyt with tecnho</p>
+</div>
+)
+
+export default Profile
